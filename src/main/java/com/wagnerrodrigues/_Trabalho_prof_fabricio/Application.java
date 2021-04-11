d00bd1de4d52bf433873c07a4fa8bf5fc8592f91
wagner rodrigues <wagner.rodrigues@acad.ufsm.br>
2021-04-11 16:31:42 -0300

    Adicionei_spring_boot_initializr

---
+++ src/main/java/com/wagnerrodrigues/_Trabalho_prof_fabricio/Application.java
@@ -0,0 +1,13 @@
+package com.wagnerrodrigues._Trabalho_prof_fabricio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagnerrodrigues/_Trabalho_prof_fabricio/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagnerrodrigues._Trabalho_prof_fabricio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
